d61d0ced2da8352641aac835a58a0160ab20f453
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-18 22:42:31 -0300

    .
    .

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,10 +31,14 @@ html{
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
   }
-
-
-
-
+  body, input, textarea, button{
+    font-family: 'Poppins' , sans-serif;
+    font-weight: 400;
+  }
   
+
+
+
+
 
 `;
