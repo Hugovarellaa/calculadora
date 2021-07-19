1e0fa2a192378a7328d12bc965add90a4ed93be4
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-18 22:41:44 -0300

    .
    .

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,2 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
+export const GlobalStyle = createGlobalStyle`
+:root{
+    --blue: #5429CC;
+    --blue-light: #6933ff;
+    --green: #33CC95;
+    --red: #E62E4D;
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
+  }
+  
+
+
+
+
+`
